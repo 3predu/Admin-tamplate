c7493554674f3c7a987d57165d2fa1398a65fe78
predu <3pedrolucca@gmail.com>
2021-12-05 20:12:31 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/tamplate/Layout";
+
+export default function Home() {
+  return (
+    <Layout titulo="Página Inicial" subtitulo="Construção de tamplate-admin">
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
